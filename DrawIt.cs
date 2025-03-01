85a2913cf1c2c3be46bd9aca22e40f8e6b01bc67
Z. Csaba Johanyák <39920181+jzscsaba@users.noreply.github.com>
2025-03-01 18:21:42 +0100

    Add files via upload

---
+++ DrawIt.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WPFVisuals
+{
+	// Host container for DrawingVisual.
+	class DrawIt : FrameworkElement
+	{
+		private readonly VisualCollection Children;
+		public DrawIt()
+		{
+			// A megjelenített vizuális objektumok (gyermek objektumok) referenciáit tárolja.
+			Children = new VisualCollection(this);
+			// A DrawIt komponens betöltődésekor történik a rajzolás.
+			Loaded += DrawIt_Loaded;
+		}
+
+		private void DrawIt_Loaded(object sender, RoutedEventArgs e)
+		{
+			int x = 0;
+			// A két négyzet megrajzolása.
+			for (int i = 0; i < 2; i++)
+			{
+				// Négyzet objektum létehozása.
+				DrawingVisual Square = new();
+				// Eszközkapcsolat megnyitása.
+				using (DrawingContext dc = Square.RenderOpen())
+				{
+					// Négyzet megrajzolása.
+					dc.DrawRectangle(Brushes.Red, new Pen(Brushes.Black, 2),
+							new Rect(new Point(0 + x, 0), new Size(40, 40)));
+				}
+				// Négyzet objektum hozzáadása a drawIt gyermekeihez.
+				_=Children.Add(Square);
+				x += 60;
+			}
+		}
+		protected override Visual GetVisualChild(int index)
+		{
+			return Children[index];
+		}
+		protected override int VisualChildrenCount => Children.Count;
+	}
+}
